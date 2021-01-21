3172333f63b44d04fdf914562a191e4a7fbe8375
Jesper <Jeppe_90@hotmail.com>
2021-01-21 13:17:11 +0100

    added table component for displaying the data

--- src/form/CustomerForm.js
+++ src/form/CustomerForm.js
@@ -1,5 +1,5 @@
-import React from "react";
-import TextInput from './TextInput';
+import React, { useState } from "react";
+import TextInput from "./TextInput";
 import { Form, Button } from "react-bootstrap";
 import * as yup from "yup";
 import { Formik } from "formik";
@@ -25,73 +25,95 @@ const initialValues = {
 };
 
 const CustomerForm = () => {
-
+  const [tableRow, setTableRow] = useState([]);
   const onFormSubmit = (values) => {
-      console.log(values)
+    setTableRow((row) => [...row, values]);
   };
 
+  console.log(tableRow);
+  //   const Table = ({ row }) => <li>{row}</li>;
+
+  const renObjData = tableRow.map(function (col, idx) {
+    return (
+      <div key={idx}>
+        <p>{col.creditorName}</p>
+        <p>{col.loanAmount}</p>
+        <p>{col.monthlyFee}</p>
+        <p>{col.apr}</p>
+      </div>
+    );
+  });
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={newSchema}
-      onSubmit={(values) => {
-        onFormSubmit(values);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleSubmit,
-        handleChange,
-        handleBlur,
-      }) => (
-        <Form onSubmit={handleSubmit}>
-          <TextInput
-            type="text"
-            name="creditorName"
-            values={values}
-            errors={errors}
-            touched={touched}
-            handleChange={handleChange}
-            handleBlur={handleBlur}
-          />
-          <TextInput
-            type="text"
-            name="loanAmount"
-            label="Description"
-            values={values}
-            errors={errors}
-            touched={touched}
-            handleChange={handleChange}
-            handleBlur={handleBlur}
-            textarea="textarea"
-          />
-          <TextInput
-            type="text"
-            name="monthlyFee"
-            label="Price (in SEK)"
-            values={values}
-            errors={errors}
-            touched={touched}
-            handleChange={handleChange}
-            handleBlur={handleBlur}
-          />
-           <TextInput
-            type="text"
-            name="apr"
-            label="Price (in SEK)"
-            values={values}
-            errors={errors}
-            touched={touched}
-            handleChange={handleChange}
-            handleBlur={handleBlur}
-          />
-          <pre>{JSON.stringify(values, null, 2)}</pre>
-          <Button type="submit">Save!</Button>
-        </Form>
-      )}
-    </Formik>
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={newSchema}
+        onSubmit={(values) => {
+          onFormSubmit(values);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <TextInput
+              type="text"
+              name="creditorName"
+              label="Creditor name"
+              values={values}
+              errors={errors}
+              touched={touched}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+            />
+            <TextInput
+              type="text"
+              name="loanAmount"
+              label="Loan amount(SEK)"
+              values={values}
+              errors={errors}
+              touched={touched}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+            />
+            <TextInput
+              type="text"
+              name="monthlyFee"
+              label="Monthly fee(SEK)"
+              values={values}
+              errors={errors}
+              touched={touched}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+            />
+            <TextInput
+              type="text"
+              name="apr"
+              label="Add APR"
+              values={values}
+              errors={errors}
+              touched={touched}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+            />
+            <Button type="submit">Save!</Button>
+          </Form>
+        )}
+      </Formik>
+      {renObjData}
+      {/* {tableRow.map((row, i) => (
+           <Table
+           row={row}
+           key={row + i}
+         />
+      ))} */}
+    </>
   );
 };
 
